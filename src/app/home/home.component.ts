fece416a5e6918bf60a7fa409292340fc5868ba0
Sachin Teckchandani <100620287@oncampus.local>
2020-03-12 17:33:17 -0400

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({ 
+styleUrls: ['style.css'], 
+templateUrl: 'home.component.html' 
+})
+export class HomeComponent {}
+
